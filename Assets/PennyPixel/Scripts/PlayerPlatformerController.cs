b18ea772f7c5672e6ba3e3135d326d1de4fb31f0
Daniel Tomas <ticidici@yahoo.es>
2018-11-22 15:20:56 +0100

    Tutorial until video 4/8. Better player control. Following https://unity3d.com/es/learn/tutorials/topics/2d-game-creation/intro-2d-world-building-w-tilemap?playlist=17093

---
+++ Assets/PennyPixel/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject {
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+    public float downwardsGravityMultiplier = 1.9f;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Use this for initialization
+    void Awake () 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer> (); 
+        animator = GetComponent<Animator> ();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis ("Horizontal");
+
+        if (Input.GetButtonDown ("Jump") && grounded) {
+            velocity.y = jumpTakeOffSpeed;
+        } else if (Input.GetButtonUp ("Jump")) 
+        {
+            if (velocity.y > 0) {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        if (!grounded && velocity.y < 0) {
+            //more gravity
+            downwardsGravityModifier = downwardsGravityMultiplier;
+        }else
+        {
+            downwardsGravityModifier = 1;
+        }
+
+        if(move.x > 0.01f)
+        {
+            if(spriteRenderer.flipX == true)
+            {
+                spriteRenderer.flipX = false;
+            }
+        } 
+        else if (move.x < -0.01f)
+        {
+            if(spriteRenderer.flipX == false)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+
+        animator.SetBool ("grounded", grounded);
+        animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+    }
+}
